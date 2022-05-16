158beca676551d3cb8d6451d50b6532f3937bb34
eden.lorrai@gmail.com <eden.lorrai@gmail.com>
2022-05-16 14:38:11 -0700

    added writing to a csv file

--- tech_spotlight/term_search.py
+++ tech_spotlight/term_search.py
@@ -1,10 +1,11 @@
 import re
+import csv
 
-with open('../datasets/placeholder.txt', 'rt') as f: # BRING IN RAW FILE
+with open('./test-raw-data/does_regex_work.txt',  'rt') as f: # BRING IN RAW FILE
   text_content = f.read()
 
 def open_term_file():
-  with open('../datasets/tech_list.txt', 'rt') as l: # BRING IN TERMS
+  with open('./datasets/tech_list.txt', 'rt') as l: # BRING IN TERMS
     list_content = l.readlines()
     tech_terms = []
     for line in list_content:
@@ -15,6 +16,7 @@ def open_term_file():
 
 def get_terms():
   term_list = open_term_file()
+  data_list = []
   for term in term_list:
     pattern = r'' + re.escape(term)
     # pattern = 
@@ -23,16 +25,23 @@ def get_terms():
     lower_match = []
     for item in match:
       lower_match.append(item.lower())
-    print(lower_match)
+    data_list.append((term, len(lower_match)))
+  return data_list
+
+header = ["Term", "Frequency"]
+data_list = get_terms()
+
+with open('./datasets/tech_term_frequency_retry.csv', 'a+') as f:
+  writer = csv.writer(f)
+  writer.writerow(header)
+  writer.writerows(data_list)
 
 '''
 1. Bring in the raw data file [X]
 2. Bring in the term list [X]
-3. loop through each key in our dictionary []
-4. create a regex for each key in our dictionary(potentially some weird neat cool regex that will match all them) []
-5. input regex key in our findall() for each dict key []
-6. increment to the dictionary for each occurrance of key []
+4. create a regex for each key in our dictionary(potentially some weird neat cool regex that will match all them) [X]
+5. input regex key in our findall() for each dict key [X]
+6. increment to the dictionary for each occurrance of key [X]
 7. write the counts+terms to new file (csv) []
 '''
 
-get_terms()
