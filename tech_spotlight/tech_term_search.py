5248325355ee61c0ecc50e44bf8d2535499d5e4e
Christopher Yamas <yamas.chris@gmail.com>
2022-05-18 18:30:38 -0400

    functions refactored, new regex added

--- tech_spotlight/tech_term_search.py
+++ tech_spotlight/tech_term_search.py
@@ -1,39 +1,61 @@
 import re
 import csv
 
-with open('./tech_spotlight/jobs_raw.txt', 'rt') as f:  # BRING IN RAW FILE
-    text_content = f.read()
+
+def open_text(read_file):
+    with open(read_file, 'rt') as f:  # BRING IN RAW FILE
+        text_content = f.read()
+    f.close()
+    return text_content
 
 
-def open_term_file():
-    with open('./datasets/tech_list.txt', 'rt') as l:  # BRING IN TERMS
-        list_content = l.readlines()
+def open_terms(terms_file):
+    with open(terms_file, 'rt') as f:  # BRING IN TERMS
+        list_content = f.readlines()
         tech_terms = []
         for line in list_content:
             line_stripped = line.strip('\n')
             tech_terms.append(line_stripped)
+    f.close()
     return tech_terms
 
 
-def get_terms():
-    term_list = open_term_file()
+def get_terms(read_file, terms_file):
+    text_content = open_text(read_file)
+    term_list = open_terms(terms_file)
     data_list = []
     match_num = 0
     for term in term_list:
         if term == "Amazon EC2":
             pattern = r'Amazon EC2\W[^C]'
+        if term == "Angular":
+            pattern = r'\WAngular\D[^jsJS]'
+        if term == "Apache Tomcat":
+            pattern = r'Tomcat'
+        elif term == "CSS 3":
+            pattern = r'CSS'
         elif term == "Docker":
             pattern = r'Docker\W[^C]'
+        elif term == "ExpressJS":
+            pattern = r'Express'
         elif term == "Git":
             pattern = r'Git[^HL]'
         elif term == "GitHub":
             pattern = r'GitHub\W[^P]'
+        elif term == "HTML5":
+            pattern = r'HTML'
         elif term == "Java":
             pattern = r'Java[^S]'
         elif term == "JQuery":
             pattern = r'JQuery\W[^U]'
+        elif term == ".NET CORE":
+            pattern = r'.NET\sC[oO][rR][eE]\W'
+        elif term == "NoSQL":
+            pattern = r'\WNoSQL\W'
         elif term == "React":
             pattern = r'React\W[^N]'
+        elif term == "SQL":
+            pattern = r'\WSQL\W'
         elif term == "Twilio":
             pattern = r'Twilio\W[^S]'
         elif term == "Visual Studio":
@@ -50,13 +72,14 @@ def get_terms():
     return data_list
 
 
-header = ["Term", "Frequency"]
-data_list = get_terms()
+def write_data(read, terms, write):
+    data_list = get_terms(read, terms)
+    header = ["Term", "Frequency"]
+    with open(write, 'w') as f:
+        writer = csv.writer(f)
+        writer.writerow(header)
+        writer.writerows(data_list)
+    f.close()
 
-with open('test_java_fix.csv', 'w') as f:
-    writer = csv.writer(f)
-    writer.writerow(header)
-    writer.writerows(data_list)
 
-########## GITUATIONS SAVE ME
-#########PANIC
+write_data('./jobs_raw.txt', './datasets/tech_list.txt', 'test_java_fix.csv')
