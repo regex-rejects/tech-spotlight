ee86438318b18289d92fce864f2bfee2d01e977c
Benjamin <benj.james.carter@gmail.com>
2022-05-18 15:43:12 -0700

    wrote more docstrings for the functions

--- tests/test_scraper.py
+++ tests/test_scraper.py
@@ -1,34 +1,5 @@
 import pytest
-from tech_spotlight.scraper import scraper, job_soup, soup_kitchen
+from tech_spotlight.scraper import scraper, job_soup, soup_kitchen, sleepy_pill, get_input, job_id_to_description
 
 
-# def test_scraper_exists():
-#     assert scraper
 
-
-def test_valid_url():
-    pass
-
-
-def test_job_soup_input():
-    pass
-
-
-def test_job_soup_output(soup_input):
-    actual = soup_input
-    expected = "<class 'bs4.BeautifulSoup'>"
-    assert isinstance(actual, 'bs4.BeautifulSoup')
-
-
-def test_soup_kitchen_input():
-    pass
-
-
-def test_soup_kitchen_output():
-    pass
-
-
-@pytest.fixture
-def soup_input():
-    input_url = 'https://www.indeed.com/viewjob?jk=19d0b31177e3c1d7'
-    return input_url
